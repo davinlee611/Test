d0a461e8c9453a34a49d91f2476b8961f6edb38d
davinlee611 <davinlee611@gmail.com>
2026-07-15 21:31:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('clientForm');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        const formData = new FormData(form);
-        const data = {};
-        
-        formData.forEach((value, key) => {
-            data[key] = value;
-        });
-        
-        console.log(data);
-        
-        // You can add additional logic here to handle the data,
-        // such as sending it to a server or displaying a confirmation message.
-    });
-});
